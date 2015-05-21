ad2eae61a878dfcde0a0e4e4adec0313c195f565
Stephen Done <srdone@gmail.com>
2015-05-21 16:18:16 -0600

    Added fluent api, equals, subtract, divide (only when the result is an integer)

--- Katas/Calculator.cs
+++ Katas/Calculator.cs
@@ -2,16 +2,61 @@ namespace Katas
 {
 	public class Calculator
 	{
-		public static int Add(string value)
-		{
-			if (value == "")
+		
+		static int addValues(string values)
+		{	
+			int result = 0;
+			foreach (int v in StringParser.parseValues(StringParser.splitNumbers(values)))
 			{
-				return 0;
+				result += v;
+			}
+			
+			return result;
+		}
+		
+		public static int Add(params string[] values)
+		{
+			int result = 0;
+			foreach (string v in values)
+			{
+				result += addValues(v);
+			}
+			return result;
+		}
+	}
+	
+	public class StringParser
+	{
+		const string emptyString = "";
+		const int emptyStringValue = 0;
+		
+		public static string[] splitNumbers(string values)
+		{
+			return values.Split(new char[] {','});
+		}
+		
+		public static int parseSingleValue(string value)
+		{
+			if (value == emptyString)
+			{
+				return emptyStringValue;
 			}
 			else
 			{
 				return int.Parse(value);
 			}
+		}
+		
+		public static int[] parseValues(string[] stringValues)
+		{
+			int[] parsedValues = new int[stringValues.Length];
+			
+			for (int i = 0; i < parsedValues.Length; i++)
+			{
+				parsedValues[i] = parseSingleValue(stringValues[i]);
+			}
+			
+			return parsedValues;
 		}
 	}
 	
--- Katas/CalculatorSpec.cs
+++ Katas/CalculatorSpec.cs
@@ -13,6 +13,11 @@ namespace Katas
 	{
 		string emptyString;
 		string seven;
+		string six;
+		string five;
+		string four;
+		string three;
+		string sevenAndSix;
 		string randomIntString;
 		int randomInt;
 		
@@ -21,6 +26,11 @@ namespace Katas
 		{
 			emptyString = "";
 			seven = "7";
+			six = "6";
+			five = "5";
+			four = "4";
+			three = "3";
+			sevenAndSix = seven + "," + six;
 			
 			var random = new Random();
 			randomInt = random.Next();
@@ -60,6 +70,47 @@ namespace Katas
 			var result = Calculator.Add(negativeRandomIntString);
 			
 			Assert.AreEqual(negativeRandomInt, result);
+		}
+		
+		[Test]
+		public void AddTwoInts()
+		{
+			var result = Calculator.Add(seven, six);
+			
+			Assert.AreEqual(13, result);
+		}
+		
+		[Test]
+		public void Add2IntsSeparatedByCommas()
+		{
+			var result = Calculator.Add(sevenAndSix);
+			
+			Assert.AreEqual(13, result);
+		}
+
+		private string concatenateWithCommas(params string[] values)
+		{
+			return String.Join(",", values);
+		}
+		
+		[Test]
+		public void Add5IntsSeparatedByCommas()
+		{
+			var sevenSixFiveFourThree = concatenateWithCommas(seven, six, five, four, three);
+			var result = Calculator.Add(sevenSixFiveFourThree);
+			
+			Assert.AreEqual(7 + 6 + 5 + 4 + 3, result);
+		}
+		
+		[Test]
+		public void Add5IntsSeparatedByCommasWithSomeNegative()
+		{
+			string negativeSeven = "-" + seven;
+			string negativeFive = "-" + five;
+			var sumString = concatenateWithCommas(negativeSeven, six, negativeFive, four, three);
+			var result = Calculator.Add(sumString);
+			
+			Assert.AreEqual(-7 + 6 + -5 + 4 + 3, result);
 		}
 	}
 }
