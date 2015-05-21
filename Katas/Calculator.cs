6c126f7ff815828dbb56876f74f5722a46a7ee75
Stephen Done <srdone@gmail.com>
2015-05-21 16:18:16 -0600

    Added fluent api, equals, subtract, divide (only when the result is an integer)

--- Katas/Calculator.cs
+++ Katas/Calculator.cs
@@ -2,17 +2,129 @@ namespace Katas
 {
 	public class Calculator
 	{
-		public static int Add(string value)
+		private int runningResult;
+		
+		public Calculator()
 		{
-			if (value == "")
+			runningResult = 0;
+		}
+		
+		static int addValues(string values)
+		{	
+			int result = 0;
+			foreach (int v in StringParser.parseValues(StringParser.splitNumbers(values)))
 			{
-				return 0;
+				result += v;
+			}
+			
+			return result;
+		}
+		
+		static int subtractValues(string values)
+		{
+			int result = 0;
+			foreach (int v in StringParser.parseValues(StringParser.splitNumbers(values)))
+			{
+				result -= v;
+			}
+			
+			return result;
+		}
+		
+		static double divideValues(string values)
+		{
+			double result = 1;
+			foreach (int v in StringParser.parseValues(StringParser.splitNumbers(values)))
+			{
+				result = (result / v);
+			}
+			
+			return result;
+		}
+		
+		public Calculator Add(params string[] values)
+		{
+			int result = 0;
+			foreach (string v in values)
+			{
+				result += addValues(v);
+			}
+			runningResult += result;
+			
+			return this;
+		}
+		
+		public int Equals()
+		{
+			return runningResult;
+		}
+		
+		public Calculator Reset()
+		{
+			runningResult = 0;
+			
+			return this;
+		}
+		
+		public Calculator Subtract(params string[] values)
+		{
+			int result = 0;
+			foreach (string v in values)
+			{
+				result += subtractValues(v);
+			}
+			runningResult += result;
+			
+			return this;
+		}
+		
+		public Calculator Divide(params string[] values)
+		{
+			double result = 1;
+			foreach (string v in values)
+			{
+				result = (int) (result / divideValues(v));
+			}
+			runningResult = (int) (runningResult / result);
+			
+			return this;
+		}
+	}
+	
+	public class StringParser
+	{
+		const string emptyString = "";
+		const int emptyStringValue = 0;
+		
+		public static string[] splitNumbers(string values)
+		{
+			return values.Split(new char[] {','});
+		}
+		
+		public static int parseSingleValue(string value)
+		{
+			if (value == emptyString)
+			{
+				return emptyStringValue;
 			}
 			else
 			{
 				return int.Parse(value);
 			}
 		}
+		
+		public static int[] parseValues(string[] stringValues)
+		{
+			int[] parsedValues = new int[stringValues.Length];
+			
+			for (int i = 0; i < parsedValues.Length; i++)
+			{
+				parsedValues[i] = parseSingleValue(stringValues[i]);
+			}
+			
+			return parsedValues;
+		}
+		
 	}
 	
 	class FakeMain
--- Katas/CalculatorSpec.cs
+++ Katas/CalculatorSpec.cs
@@ -13,24 +13,45 @@ namespace Katas
 	{
 		string emptyString;
 		string seven;
+		string six;
+		string five;
+		string four;
+		string three;
+		string two;
+		string one;
+		string sevenAndSix;
 		string randomIntString;
 		int randomInt;
+		Calculator calculator;
+		
+		[TestFixtureSetUp]
+		public void Init()
+		{
+			emptyString = "";
+			seven = "7";
+			six = "6";
+			five = "5";
+			four = "4";
+			three = "3";
+			two = "2";
+			one = "1";
+			sevenAndSix = seven + "," + six;
+		}
 		
 		[SetUp]
 		public void init()
 		{
-			emptyString = "";
-			seven = "7";
-			
 			var random = new Random();
 			randomInt = random.Next();
 			randomIntString = randomInt.ToString();
+			
+			calculator = new Calculator();
 		}
 		
 		[Test]
 		public void AddEmptyString()
 		{
-			var result = Calculator.Add(emptyString);
+			var result = calculator.Add(emptyString).Equals();
 			
 			Assert.AreEqual(0, result);
 		}
@@ -38,7 +59,7 @@ namespace Katas
 		[Test]
 		public void AddSeven()
 		{
-			var result = Calculator.Add(seven);
+			var result = calculator.Add(seven).Equals();
 			
 			Assert.AreEqual(7, result);
 		}
@@ -46,7 +67,7 @@ namespace Katas
 		[Test]
 		public void AddRandomInt()
 		{
-			var result = Calculator.Add(randomIntString);
+			var result = calculator.Add(randomIntString).Equals();
 			
 			Assert.AreEqual(randomInt, result);
 		}
@@ -57,9 +78,177 @@ namespace Katas
 			var negativeRandomInt = -randomInt;
 			var negativeRandomIntString = "-" + randomIntString;
 			
-			var result = Calculator.Add(negativeRandomIntString);
+			var result = calculator.Add(negativeRandomIntString).Equals();
 			
 			Assert.AreEqual(negativeRandomInt, result);
+		}
+		
+		[Test]
+		public void AddTwoInts()
+		{
+			var result = calculator.Add(seven, six).Equals();
+			
+			Assert.AreEqual(13, result);
+		}
+		
+		[Test]
+		public void Add2IntsSeparatedByCommas()
+		{
+			var result = calculator.Add(sevenAndSix).Equals();
+			
+			Assert.AreEqual(13, result);
+		}
+
+		private string concatenateWithCommas(params string[] values)
+		{
+			return String.Join(",", values);
+		}
+		
+		[Test]
+		public void Add5IntsSeparatedByCommas()
+		{
+			var sevenSixFiveFourThree = concatenateWithCommas(seven, six, five, four, three);
+			
+			var result = calculator.Add(sevenSixFiveFourThree).Equals();
+			
+			Assert.AreEqual(7 + 6 + 5 + 4 + 3, result);
+		}
+		
+		[Test]
+		public void Add5IntsSeparatedByCommasWithSomeNegative()
+		{
+			string negativeSeven = "-" + seven;
+			string negativeFive = "-" + five;
+			var sumString = concatenateWithCommas(negativeSeven, six, negativeFive, four, three);
+			
+			var result = calculator.Add(sumString).Equals();
+			
+			Assert.AreEqual(-7 + 6 + -5 + 4 + 3, result);
+		}
+		
+		[Test]
+		public void CanRunAddTwiceAndGetCorrectResult()
+		{
+			calculator.Add(five);
+			calculator.Add(seven);
+			int result = calculator.Equals();
+			
+			Assert.AreEqual(12, result);
+		}
+		
+		[Test]
+		public void EqualsShouldNotResetTotal()
+		{
+			calculator.Add(five);
+			int result1 = calculator.Equals();
+			calculator.Add(seven);
+			int result2 = calculator.Equals();
+			
+			Assert.AreEqual(5, result1);
+			Assert.AreEqual(12, result2);
+		}
+		
+		[Test]
+		public void ResetShouldResetCalculatorToZero()
+		{
+			calculator.Add(five);
+			calculator.Reset();
+			int result = calculator.Equals();
+			
+			Assert.AreEqual(0, result);
+		}
+		
+		[Test]
+		public void ResetShouldReturnTheCalculatorInstance()
+		{
+			Calculator calculator2 = calculator.Reset();
+			
+			Assert.AreSame(calculator, calculator2);
+		}
+		
+		[Test]
+		public void TestComplexFluentAPI()
+		{
+			int result = calculator.Add(five).Add(seven).Reset().Add(three).Equals();
+			
+			Assert.AreEqual(3, result);
+		}
+		
+		[Test]
+		public void SubtractShouldSubtractTheValueProvided()
+		{
+			int result = calculator.Subtract(five).Equals();
+			
+			Assert.AreEqual(-5, result);
+		}
+		
+		[Test]
+		public void SubtractShouldBeFluent()
+		{
+			int result = calculator.Add(three).Subtract(four).Equals();
+			
+			Assert.AreEqual(-1, result);
+		}
+		
+		[Test]
+		public void SubtractShouldReturnTheCalculatorInstance()
+		{
+			Calculator calculator2 = calculator.Subtract(four);
+			
+			Assert.AreSame(calculator, calculator2);
+		}
+		
+		[Test]
+		public void SubtractShouldHandleNegativeValues()
+		{
+			string negativeFour = "-" + four;
+			int result = calculator.Subtract(negativeFour).Equals();
+			
+			Assert.AreEqual(4, result);
+		}
+		
+		[Test]
+		public void SubtractShouldHandleMultipleValues()
+		{
+			string fourAndFive = concatenateWithCommas(four, five);
+			int result = calculator.Subtract(fourAndFive).Equals();
+			
+			Assert.AreEqual(-9, result);
+		}
+		
+		[Test]
+		public void DivideShouldReturnTheCalculatorInstance()
+		{
+			Calculator calculator2 = calculator.Divide(seven);
+			
+			Assert.AreSame(calculator, calculator2);
+		}
+		
+		[Test]
+		public void DivideShouldDivideTheCurrentRunningValueByProvidedValue()
+		{
+			int result1 = calculator.Divide(seven).Equals();
+			int result2 = calculator.Add(seven).Divide(seven).Equals();
+			
+			Assert.AreEqual(0, result1);
+			Assert.AreEqual(1, result2);
+		}
+		
+		[Test]
+		public void DivideShouldTakeMultipleValues()
+		{
+			string twoAndTwo = concatenateWithCommas(two, two);
+			calculator.Add(four).Divide(twoAndTwo);
+			int result = calculator.Equals();
+			
+			Assert.AreEqual(1, result);
+		}
+		
+		[Test]
+		[Ignore("Deciding how to implement non-integers")]
+		public void DivideShouldHandleResultsProducingRealNumbers()
+		{
+			
 		}
 	}
 }
